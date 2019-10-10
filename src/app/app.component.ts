8dd34874da97faf5ebde7a32c2f66a016febe422
mireiaGomariz <mireiapgomariz@gmail.com>
2019-10-10 13:21:51 +0200

    Json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,14 @@ export class AppComponent {
   a = 0.234;
 
   salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
 }
